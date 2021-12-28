2edf1ae8cac14a32ba0de7cd97031336c5b97fb5
eAldarn <berteaux.cedric@gmail.com>
2021-12-28 21:22:42 +0100

    Full pipelin test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Salut le monde !'));
+app.get('/', (req, res) => res.send('Salut le monde ! Voilà la modification du module 5 !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
